f5781c338fa49937bd5a04631dfffd0588008643
thanhpv00921 <thanhpv00921@gmail.com>
2025-03-01 09:04:48 +0000

    modified:   load_and_predict_CSBE.py 	modified:   load_and_predict_SMOTED_RF.py 	modified:   requirements.txt 	modified:   streamlit_app.py 	modified:   train_CSBE.py

--- load_and_predict_SMOTED_RF.py
+++ load_and_predict_SMOTED_RF.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from scipy import stats
 
 from sklearn.ensemble import RandomForestClassifier
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import math
 import numpy as np
 
