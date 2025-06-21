651d3f1d5708e8445e22ecca781c27d6e2290253
thanhpv00921 <thanhpv00921@gmail.com>
2025-06-21 00:59:09 +0000

    new file:   models/multiclass_FDP_VN_1year_DT.pkl 	new file:   models/multiclass_FDP_VN_1year_GB.pkl 	new file:   models/multiclass_FDP_VN_1year_MLR.pkl 	new file:   models/multiclass_FDP_VN_1year_RF.pkl 	new file:   models/multiclass_FDP_VN_1year_SVM.pkl 	new file:   models/scaler_params.pkl

--- load_and_predict_GB.py
+++ load_and_predict_GB.py
@@ -7,7 +7,17 @@ import pickle
 with open('models/multiclass_FDP_VN_1year_GB.pkl', 'rb') as f:
     model = pickle.load(f)
 
+with open('models/scaler_params.pkl', 'rb') as f:
+    scaler_params = pickle.load(f)
+mean = scaler_params['mean']
+std = scaler_params['std']
+
 # predict a single sample
 def predict_a_sample_GB(x):
+    print(mean)
+    print(std)
+    print(x)
+    x = (x - mean) / std
+    print(x)
     y_pred = model.predict(x.reshape(1, -1))
     return y_pred
